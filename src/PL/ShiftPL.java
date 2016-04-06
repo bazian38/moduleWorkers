afb22a9288075cb4808922b9f669605f58aeb28b
bazian38 <bazian@post.bgu.ac.il>
2016-04-06 21:17:04 +0300

    ShiftPL

---
+++ src/PL/ShiftPL.java
@@ -0,0 +1,26 @@
+package PL;
+
+import BL.ShiftBL;
+import BackEnd.Shift;
+
+import java.util.LinkedList;
+
+/**
+ * Created by ahmad on 4/6/2016.
+ */
+public class ShiftPL {
+    private ShiftBL BL;
+
+    public ShiftPL(){
+        BL=new ShiftBL();
+    }
+
+
+
+    public void Run(){
+
+
+    }
+
+
+}
